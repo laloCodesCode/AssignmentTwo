27f4b2b4a07fff35cac0bcfd8855e8fe1c74af65
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 12:38:22 -0500

    added more one more var

--- src/main/java/CSC340/AssignmentTwo/weatherData.java
+++ src/main/java/CSC340/AssignmentTwo/weatherData.java
@@ -6,16 +6,20 @@ public class weatherData {
     private double temprature;
     private double highTemp;
     private double lowTemp;
+    private double realFeel;
     private int windSpeed;
 
+
     public weatherData(String cityName,String dayOfTheWeek, double temprature, double highTemp, double lowTemp,
-    int windSpeed){
+    int windSpeed, int realFeel){
         this.cityName = cityName;
         this.dayOfTheWeek = dayOfTheWeek;
         this.temprature = temprature;
         this.highTemp = highTemp;
         this.lowTemp = lowTemp;
         this.windSpeed = windSpeed;
+        this.realFeel = realFeel;
+
 
 
     }
