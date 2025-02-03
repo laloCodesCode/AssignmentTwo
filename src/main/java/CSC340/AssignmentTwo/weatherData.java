83a414e5263d3a7faf206864c372d8cf409942c4
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 12:03:20 -0500

    Added the constructor and inited some parts of data

--- src/main/java/CSC340/AssignmentTwo/weatherData.java
+++ src/main/java/CSC340/AssignmentTwo/weatherData.java
@@ -5,4 +5,11 @@ public class weatherData {
     private String dayOfTheWeek;
     private double temprature;
 
+    public weatherData(String cityName,String dayOfTheWeek, double temprature){
+        this.cityName = cityName;
+        this.dayOfTheWeek = dayOfTheWeek;
+        this.temprature = temprature;
+
+
+    }
 }
