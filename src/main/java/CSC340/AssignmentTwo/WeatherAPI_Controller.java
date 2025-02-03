e98485f739353040caf83fd43d0a12d63c8de1e1
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 15:48:35 -0500

    Added imports to make the controller work

--- src/main/java/CSC340/AssignmentTwo/WeatherAPI_Controller.java
+++ src/main/java/CSC340/AssignmentTwo/WeatherAPI_Controller.java
@@ -1,4 +1,10 @@
 package CSC340.AssignmentTwo;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
 public class WeatherAPI_Controller {
 }
