1dc8c049a93d8b26531ec56247ced23f2247bbd3
Eduardo <eduardoherrerabarra@gmail.com>
2025-02-03 12:07:43 -0500

    Added more data for the watherAPI

--- src/main/java/CSC340/AssignmentTwo/weatherData.java
+++ src/main/java/CSC340/AssignmentTwo/weatherData.java
@@ -4,11 +4,15 @@ public class weatherData {
     private String cityName;
     private String dayOfTheWeek;
     private double temprature;
+    private double high;
+    private double low;
 
-    public weatherData(String cityName,String dayOfTheWeek, double temprature){
+    public weatherData(String cityName,String dayOfTheWeek, double temprature, double high, double low){
         this.cityName = cityName;
         this.dayOfTheWeek = dayOfTheWeek;
         this.temprature = temprature;
+        this.high = high;
+        this.low = low;
 
 
     }
